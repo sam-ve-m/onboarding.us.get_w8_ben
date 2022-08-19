1c07db5cbfe705cfc60e7492e4243ebece21460c
Samuel Veloso Marçal <svm@lionx.com.br>
2022-08-19 15:48:31 -0300

    chore: added nexus credentials

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 fission spec init
 fission env create --spec --name get-w8-ben-env --image nexus.sigame.com.br/fission-async:0.1.6 --builder nexus.sigame.com.br/fission-builder-3.8:0.0.1
-fission fn create --spec --name get-w8-ben-fn --env get-w8-ben-env --src "./func/*" --entrypoint main.get_w8_ben
-fission route create --spec --name get-w8-ben-rt --method GET --url /get-w8-ben --function get-w8-ben-fn
+fission fn create --spec --name get-w8-ben-fn --env get-w8-ben-env --src "./func/*" --entrypoint main.get_w8_ben --executortype newdeploy --maxscale 1
+fission route create --spec --name get-w8-ben-rt --method GET --url /onboarding/get-w8-ben --function get-w8-ben-fn
